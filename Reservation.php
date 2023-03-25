9462e84d983df797d5e4800b4cdcb579ad0b1906
Frederic Branca <frederic.branca@gmail.com>
2023-03-25 17:46:11 +0100

    correction getter setter

--- Reservation.php
+++ Reservation.php
@@ -5,52 +5,58 @@ class Reservation {
     private Client $_client;
     private string $_dateDebut;
     private string $_dateFin;
-    private Hotel $_hotel;
 
     //Constructeur pour définir une réservation//
-    private function __construct(Chambre $chambre, Client $client, string $dateDebut, string $dateFin)
+    public function __construct(Chambre $chambre, Client $client, string $dateDebut, string $dateFin)
     {
         $this->_chambre = $chambre;
         $this->_client = $client;
-        $this->_dateDebut = new DateTime($dateDebut);
-        $this->_dateFin = new DateTime($dateFin);
-        $this->_hotel->addReservation($this);
+        $this->_dateDebut = $dateDebut;
+        $this->_dateFin = $dateFin;
+        $this->_chambre->getHotel()->addReservation($this);
+        $this->_client->addReservation($this);
     }
 
     //Getter et Setter//
-    private function getChambre(): Chambre
+    public function getChambre(): Chambre
     {
         return $this->_chambre;
     }
-    private function setChambre(Chambre $chambre)
+    public function setChambre(Chambre $chambre)
     {
         $this->_chambre = $chambre;
     }
 
-    private function getClient(): Client
+    public function getClient(): Client
     {
         return $this->_client;
     }
-    private function setClient(Client $client)
+    public function setClient(Client $client)
     {
         $this->_client = $client;
     }
 
-    private function getDateDebut(): string
+    public function getDateDebut(): string
     {
         return $this->_dateDebut;
     }
-    private function setDateDebut(string $dateDebut)
+    public function setDateDebut(string $dateDebut)
     {
         $this->_dateDebut = new DateTime($dateDebut);
     }
 
-    private function getDateFin(): string
+    public function getDateFin(): string
     {
         return $this->_dateFin;
     }
-    private function setDateFin(string $dateFin)
+    public function setDateFin(string $dateFin)
     {
         $this->_dateFin = new DateTime($dateFin);
+    }
+
+    //Méthode __toString (client, hotel et chambre)
+    public function __toString()
+    {
+        return $this->_client . " " . $this->_chambre->getHotel() . " " . $this->_chambre;
     }
 }
