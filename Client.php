99ae296e05ca1b0ae38ded29f1bb161d628136c5
Frederic Branca <frederic.branca@gmail.com>
2023-03-25 17:43:53 +0100

    add table reservation[]

--- Client.php
+++ Client.php
@@ -4,12 +4,14 @@ class Client
 {
     private string $_nom;
     private string $_prenom;
+    private array $_reservation;
 
                 //Constructeur pour définir un client//
     public function __construct(string $nom, string $prenom)
     {
         $this->_nom = $nom;
         $this->_prenom = $prenom;
+        $this->_reservation = [];
     }
 
                 //Getter et Setter//
@@ -29,6 +31,12 @@ class Client
     public function setPrenom(string $prenom)
     {
         $this->_prenom = $prenom;
+    }
+
+                //Méthode pour ajouter une résa à un client//
+    public function addReservation(Reservation $reservation)
+    {
+        $this->_reservation[] = $reservation;
     }
 
                 //Méthode __toString pour afficher le nom et prenom du client//
